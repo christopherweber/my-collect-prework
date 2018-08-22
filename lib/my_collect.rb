59f800197af5a71a7af8096cdd66281cb78c4f74
Christopher Weber <chrisrrweber@gmail.com>
2018-08-22 21:36:16 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -9,15 +9,15 @@ def my_collect(array)
   new_arr
 end
 
-def my_collect(array)
-  new_arr = [] 
+# def my_collect(array)
+#   new_arr = [] 
   
-  i=0 
-  while i < array.length 
-  new_arr.push yield(array[i])
-  i += 1 
-end 
-  new_arr
-end 
+#   i=0 
+#   while i < array.length 
+#   new_arr.push yield(array[i])
+#   i += 1 
+# end 
+#   new_arr
+# end 
 
 
